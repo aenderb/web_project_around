e941bc8e1d0955c716c120c3bab6a9419f7529b8
aenderbinoto <aederb@hotmail.com>
2025-11-03 10:06:40 -0300

    fix: config.example

--- src/config.example.js
+++ src/config.example.js
@@ -1,4 +1,4 @@
-//exemplo de config.js
+//exemplo de config.js que deve ser criado localmente
 export const CONFIG = {
   API_TOKEN: "INSIRA SUA CHAVE AQUI",
   BASE_URL: "https://around-api.pt-br.tripleten-services.com/v1",
